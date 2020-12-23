69146b81d4532c46bf7bf772920ac9a3ef353999
Govind <govinddevika4@gmail.com>
2020-12-23 12:35:58 +0530

    Almost finish

--- tictactoe.py
+++ tictactoe.py
@@ -10,8 +10,11 @@ def instructions():
     print("\t\t\t7 | 8  | 9  ")
     print("\n-------------------------------------------------------")
 
-def input_values(markp,pos):
+def input_values(markp,markc,pos):
     positon =  pos -1
+    if value[positon]==markc:
+        print("\t\t\timpossible")
+        exit(0)
     value[positon] = markp
 
 def computer(markp,markc):
@@ -29,23 +32,25 @@ def computer(markp,markc):
         for i in range(first,last,skip):
             if value[i] == " ":
                 value[i] = markc
+            else:
+                continue
 
     def comp_logic(mark,id = 2):
-            if row1.count(mark)==2:
+            if row1.count(mark)==2 and row1.count(" ") != 0:
                 scan(0,3,1)
-            elif row2.count(mark)==2:
+            elif row2.count(mark)==2 and row2.count(" ") != 0:
                 scan(3,6,1)
-            elif row3.count(mark)==2:
+            elif row3.count(mark)==2 and row3.count(" ") != 0:
                 scan(6,9,1)
-            elif col1.count(mark)==2:
+            elif col1.count(mark)==2 and col1.count(" ") != 0:
                 scan(0,7,3)
-            elif col2.count(mark)==2:
+            elif col2.count(mark)==2 and col2.count(" ") != 0:
                 scan(1,8,3)
-            elif col3.count(mark)==2:
+            elif col3.count(mark)==2 and col3.count(" ") != 0:
                 scan(2,9,3)
-            elif dia1.count(mark)==2:
+            elif dia1.count(mark)==2 and dia1.count(" ") != 0:
                 scan(0,9,4)
-            elif dia2.count(mark)==2:
+            elif dia2.count(mark)==2 and dia2.count(" ") != 0:
                 scan(2,7,2)
             else:
                 if id == 1:
@@ -58,7 +63,7 @@ def computer(markp,markc):
                             value[i] = markc
                             break
                 return False
-                
+
     if comp_logic(markc,1):
         comp_logic(markp)
        
@@ -68,6 +73,7 @@ def check_result(markp,markc):
                 print("you won")
         else:
             print("compuer won")
+            exit(0)
 
     for i in range(0,7,3):
         if (value[i]==value[i+1]== value[i+2]) and value[i] != " ":                  
@@ -86,6 +92,9 @@ def check_result(markp,markc):
     if (value[2]==value[4]==value[6]) and value[2] != " ": 
         who(2)
         return False 
+    if value.count(" ") == 0:
+        print("Draw")
+        return False
     return True
 
         
@@ -109,15 +118,16 @@ loop = True
 while(loop):
     if user == "X":
         choice = int(input("\nEnter your choice: "))
-        input_values(user,choice)
+        input_values(user,AI,choice)
         computer(user, AI)
         display()
         loop = check_result(user,AI)
     else:
         computer(user, AI)
         display()
+        loop = check_result(user,AI)
         choice = int(input("\nEnter your choice: "))
-        input_values(user,choice)        
+        input_values(user,AI,choice)        
         loop = check_result(user,AI)
 
 
